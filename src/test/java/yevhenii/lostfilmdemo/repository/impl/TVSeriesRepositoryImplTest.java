88706c4cb4db52b5cc229b5d84fdbbc1b6e345b3
yevhenii <wertblek1@gmail.com>
2021-12-20 11:06:42 +0200

    test container is working

--- src/test/java/yevhenii/lostfilmdemo/repository/impl/TVSeriesRepositoryImplTest.java
+++ src/test/java/yevhenii/lostfilmdemo/repository/impl/TVSeriesRepositoryImplTest.java
@@ -1,24 +1,30 @@
 package yevhenii.lostfilmdemo.repository.impl;
 
-import org.junit.ClassRule;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
-//@ActiveProfiles("test")
+@ActiveProfiles("test")
 @SpringBootTest
 class TVSeriesRepositoryImplTest {
 
     @Autowired
     TVSeriesRepositoryImpl repository;
 
-    @ClassRule
-    public static MySQLContainer mySQLContainer = new MySQLContainer("mysql:latest") //Raw use of parameterized class 'MySQLContainer'
-            .withDatabaseName("integration-tests-db")
-            .withUsername("root")
-            .withPassword("root2021");
+    @BeforeAll
+    static void setUp(){
+        try (MySQLContainer<?> mySQLContainer = new MySQLContainer<>(DockerImageName.parse("mysql:8.0.24"))
+                .withDatabaseName("lostfilm")
+                .withUsername("root")
+                .withPassword("root2021")){
+            mySQLContainer.start();
+        }
 
+    }
     @Test
     void create() {
 
--- src/test/java/yevhenii/lostfilmdemo/services/impl/FeedServiceImplTest.java
+++ src/test/java/yevhenii/lostfilmdemo/services/impl/FeedServiceImplTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.ActiveProfiles;
 import yevhenii.lostfilmdemo.LostfilmDemoApplication;
 import yevhenii.lostfilmdemo.entity.FeedMessage;
 import yevhenii.lostfilmdemo.services.FeedService;
@@ -17,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @WireMockTest
 @AutoConfigureWireMock
 @SpringBootTest(classes = LostfilmDemoApplication.class)
-//@ActiveProfiles("test")
+@ActiveProfiles("test")
 class FeedServiceImplTest {
 
     @Autowired
